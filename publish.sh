f9afa486cb62a6392716c00ca3f2043241010a3c
Joop Ringelberg <joop.ringelberg@gmail.com>
2023-09-25 15:43:49 +0200

    Restoring resurces and others
    On backlinks from a public role.
    Write protection in Apache is no longer necessary.

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 # Publish the tutorial to inplace.works
 
-scp -r ./output/* joop@inplace.works:/var/www/mycontexts.com/techdoc
+scp -r ./output/* joop@mycontexts.com:/var/www/mycontexts.com/techdoc
